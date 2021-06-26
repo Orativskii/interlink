c71db13de1aaf48a1d233b83645bda74fe53d13a
ORATIVSKII <soratik35@gmail.com>
2021-06-26 11:24:18 +0300

    Фикс багов. форматирование кода

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const parseCsvFile = () => {
     const fileInput = document.getElementById('file-input');
-
     let uniqueNames = [];
     let uniqueDates = [];
     let reformatUniqueDates = [];
@@ -8,24 +7,22 @@ const parseCsvFile = () => {
     let headers;
     Papa.parse(fileInput.files[0], {
         complete: function (results) {
+            results.data.shift();
             uniqueNames = findUniqueData(results.data, 0);
-            uniqueDates = findUniqueData(results.data, 1)
+            uniqueDates = findUniqueData(results.data, 1);
             workerList = setWorkersList(results.data, uniqueNames);
             uniqueDates.forEach(elem => {
                 reformatUniqueDates.push(reformatDate(elem));
             })
-
             headers = createHeaders(reformatUniqueDates);
             result(workerList, headers, uniqueNames, uniqueDates)
-
         }
     });
+    fileInput.value = '';
 }
 
-
-
 const result = (workerList, headers, names, dates) => {
-    let result = "";
+    let result = '';
 
     headers.forEach((header, index) => {
         result += header + (index !== headers.length - 1 ? ',' : '');
@@ -34,16 +31,13 @@ const result = (workerList, headers, names, dates) => {
 
     names.forEach(name => {
         result += name + ',';
-        let worker = workerList.filter(elem => elem.name === name)
+        let worker = workerList.filter(elem => elem.name === name);
         let fullWorkerInfo = findElement(worker, dates);
-        // fullWorkerInfo = Object.assign({}, ...fullWorkerInfo);
         dates.forEach((workDay, index) => {
-            console.log(fullWorkerInfo[index])
-            result += fullWorkerInfo[index][workDay] + (index !== fullWorkerInfo.length -1  ? ',' : '');
+            result += fullWorkerInfo[index][workDay] + (index !== fullWorkerInfo.length - 1 ? ',' : '');
         });
         result += '\n'
     })
-    console.log(result)
     downloadResultFile(result);
 }
 
@@ -51,7 +45,6 @@ function downloadResultFile(text) {
     let element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', 'result.csv');
-
     element.click();
 }
 
@@ -61,11 +54,10 @@ const findElement = (workers, dates) => {
         return {
             [date]: person?.workHours || '0'
         }
-    })
+    });
 }
 
 const findUniqueData = (data, i) => {
-    data.shift();
     const uniqueData = [];
     data.forEach(elem => {
         if (!uniqueData.includes(elem[i])) {
@@ -76,11 +68,6 @@ const findUniqueData = (data, i) => {
 }
 
 const reformatDate = (date) => {
-    let options = {
-        day: 'numeric',
-        month: 'numeric',
-        year: 'numeric'
-    }
     let d = new Date(date);
     let year = d.getFullYear();
     let month = d.getMonth() + 1;
@@ -97,7 +84,6 @@ const reformatDate = (date) => {
 const setWorkersList = (data, uniqueNames) => {
     const workersList = [];
     uniqueNames.forEach(name => {
-
         data.forEach(elem => {
             if (elem[0] === name) {
                 workersList.push({name: name, date: elem[1], workHours: elem[2]});
